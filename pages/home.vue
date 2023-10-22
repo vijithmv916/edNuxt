9f72e588fa1e92ac21b05d96665e0b3a5815cf95
vijithmv916 <miniboyviju916@gmail.com>
2023-10-22 13:50:55 +0530

    init

---
+++ pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>this is new HOme</h1>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-slate-100 min-h-screen p-32 font-sans flex flex-col justify-center items-center">
+        <img src="https://nuxt.com/assets/design-kit/icon-green.svg" alt="" width="200">
+        <div class="text-4xl font-bold">
+            <h1>Welcome to the homepage</h1>
+        </div>
+        <div class="flex gap-4">
+            <nuxt-link to="/about">About</nuxt-link>
+            <nuxt-link to="/home">Home</nuxt-link>
+        </div>
+
+    </div>
+</template>
