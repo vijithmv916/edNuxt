82e42fa7431446bf7e2bf84e041d5df606747734
vijithmv916 <miniboyviju916@gmail.com>
2023-10-23 11:37:01 +0530

    new

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
-<<<<<<< HEAD
   ssr: true,
   app:{
     baseURL: '/edNuxt/'
   }
-=======
-  nitro: {
-    preset: 'github-pages'
-  },
->>>>>>> 3585c51d6aeef77db3b0d070238fde0937c3f8a3
 })
