149e1905b3bc98522a44d5b214c4a5b26aa6a365
vijithmv916 <miniboyviju916@gmail.com>
2023-10-23 11:32:43 +0530

    base fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
+  ssr: true,
+  app:{
+    baseURL: '/edNuxt/'
+  }
 })
